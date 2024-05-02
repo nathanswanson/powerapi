0b720d3a0c326149dbbb0d82a750457eedda7fcb
Nathan Swanson <nathanswanson370@gmail.com>
2024-05-02 07:05:29 -0500

    fix api

--- src/util/world_to_block.ts
+++
@@ -1,5 +0,0 @@
-import { Block } from "@minecraft/server";
-
-export function asWorldProperty(property: string, block: Block) {
-  return `${block.location.x}-${block.location.y}-${block.location.z}-${property}`;
-}
